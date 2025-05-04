d36c46b73496ce40ef1e022c86b9b857f3b6a1b8
Victoria Jury <victoriaehodges@gmail.com>
2025-05-04 19:14:06 +0200

    test fetch data from api server

--- react_client/src/App.jsx
+++ react_client/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:5000/api/guests');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.log('Error:', error)
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Guests</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {data.map((element, index) => {
+            // {console.log(element.name)}
+            return(
+            <div  key={index}>
+              <p>ID: #{element.id}<br/>
+              Name: {element.name}<br/>
+              Address: {element.address_1}, {element.address_2},{element.city}, {element.postcode}</p>
+            </div>
+            ) 
+        })}
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
