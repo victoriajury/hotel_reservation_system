26552f52c8cb58a495b30c1fe834f3ad4fecaa1c
Victoria Jury <victoriaehodges@gmail.com>
2025-06-11 22:39:26 +0200

    add crud endpoints to users, update tests

--- server/resources/users.py
+++ server/resources/users.py
@@ -1,7 +1,17 @@
-from flask import jsonify, make_response, request
-from flask_restful import Resource
+from flask import jsonify, make_response
+from flask_restful import HTTPException, Resource, reqparse
 from server.database import db
 from server.models import Users
+from werkzeug.exceptions import NotFound
+from werkzeug.security import generate_password_hash
+
+parser = reqparse.RequestParser()
+required_fields = [
+    "username",
+    "password",
+]
+for arg in required_fields:
+    parser.add_argument(arg, required=True)
 
 
 class UserResource(Resource):
@@ -9,24 +19,67 @@ class UserResource(Resource):
         if user_id is None:
             query = db.session.execute(db.select(Users)).scalars()
             users = [data.to_dict() for data in query.all()]
-
             return jsonify(users)
 
         else:
-            user = (
-                db.session.execute(db.select(Users).filter_by(id=user_id))
-                .scalar_one()
-                .to_dict()
-            )
-
-            return jsonify(user)
+            try:
+                user = db.get_or_404(Users, user_id).to_dict()
+                return jsonify(user)
+            except NotFound:
+                response = make_response("User not found.", 404)
+                return response
 
     def post(self):
-        form_json = request.get_json()
-        new_user = Users(username=form_json["username"], email=form_json["password"])
+        try:
+            fields = parser.parse_args()
 
-        db.session.add(new_user)
+            new_user = Users(
+                username=fields["username"],
+                password=generate_password_hash(fields["password"]),
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            response = make_response(new_user.to_dict(), 201)
+
+        except HTTPException as e:
+            response = make_response(e.data, e.code)
+
+        return response
+
+    def put(self, user_id):
+        try:
+            user = db.get_or_404(Users, user_id)
+        except NotFound:
+            response = make_response("User not found.", 404)
+            return response
+
+        try:
+            fields = parser.parse_args()
+
+            user.username = fields["username"]
+            user.password = generate_password_hash(fields["password"])
+
+            db.session.commit()
+
+            response = make_response(user.to_dict(), 204)
+
+        except HTTPException as e:
+            response = make_response(e.data, e.code)
+
+        return response
+
+    def delete(self, user_id):
+        try:
+            user = db.get_or_404(Users, user_id)
+        except NotFound:
+            response = make_response("User not found.", 404)
+            return response
+
+        db.session.delete(user)
         db.session.commit()
 
-        response = make_response(new_user.to_dict(), 201)
+        response = make_response("User deleted", 204)
+
         return response
