ff4bf7202bdc99355dc150c1fc0ed3f1b6909bc6
Victoria Jury <victoriaehodges@gmail.com>
2025-06-14 22:38:58 +0200

    Implement user data fetching and loader; update Users page to display user data in table format

--- react_client/src/pages/Users.tsx
+++ react_client/src/pages/Users.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { getUsers } from '../data/users';
+
 import Box from '@mui/joy/Box';
 import Button from '@mui/joy/Button';
 import Typography from '@mui/joy/Typography';
@@ -8,9 +11,17 @@ import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 import DataTable from '../layouts/components/DataTable';
 import DataList from '../layouts/components/DataList';
 
-export default function UsersPage() {
-  
+export async function loader() {
+  const users = await getUsers();
+  return { users };
+}
 
+const usersColumns = [
+  { key: 'username', label: 'Username' },
+];
+
+export default function UsersPage() {
+  const { users } = useLoaderData();
   return (
     <>
       <Box
@@ -36,7 +47,7 @@ export default function UsersPage() {
         </Button>
       </Box>
       {/* Desktop View */}
-      <DataTable />
+      <DataTable data={users} columns={usersColumns} />
       {/* Mobile View */}
       <DataList />
     </>
